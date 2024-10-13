ae777ae4a104a56f70d8da4c74250b6792110860
LordDannyKay <70198125+lorddannykay@users.noreply.github.com>
2024-10-13 12:50:14 +0530

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Nav } from "@/components/Nav";
 import { cn } from "@/utils";
 
 export const metadata: Metadata = {
-  title: "Hume AI - EVI - Next.js Starter",
-  description: "A Next.js starter using Hume AI's Empathic Voice Interface",
+  title: "EverCoach AI",
+  description: "Your personal AI Coach at Foundever ",
 };
 
 export default function RootLayout({
